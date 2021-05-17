96a57846b17b30f52e871d7a14f3f48582e3d699
DVILLA <dvilla@unal.edu.co>
2021-05-16 19:45:45 -0500

    Reto diario

--- main.r
+++ main.r
@@ -1,4 +1,4 @@
-```{r}
+## ------------------------------------------------------------------------------
 "Protocolo:
 
  1. Daniel Felipe Villa Rengifo
@@ -9,24 +9,9 @@
 
  4. Fuentes:
     https://www.generatedata.com"
-```
 
 
-# Ejercicio 1
-
-Vamos a coger el nombre de una persona y su región, y en otra columna una dirección de correo electronico,
-
-+ Ejemplo:
-
-Nombre| Región | @---.com   |  
-------|--------|------------|
-Jose  | Brazil | @gmail.com |
-
-+ para convertirlo en:
-
-Jose_Brazil@gmail.com
-
-```{r}
+## ------------------------------------------------------------------------------
 #Importamos la base de datos:
 base1 <- read.csv(file = "Base1.csv", header = T, sep = ",", dec = ".")
 
@@ -61,24 +46,9 @@ su_correo <- data.frame(
 
 # Exportamos el vector:
 write.table(su_correo, file = "CorreoElectronico.txt", row.names = F)
-```
-
-# Ejercicio 2
-
-Vamso a cambiar una base de datos de los empleados donde se contiene:
-
-+ Nombres
-+ Productura donde trabaja
-+ Banco donde se le consigna
-
-Vamos a cambiar aquellas empresas como:
-
-1. Fox, HBO, Marvel por aquella quien la compro = Disney
-
-2. Vamos a remplazar Colpatria = Scotiabank y WWB = BancoW
 
 
-```{r}
+## ------------------------------------------------------------------------------
 # Cargamos la base de datos:
 base2 <- read.csv(file = "Base2.csv", sep = ",", header = T)
 
@@ -86,15 +56,14 @@ base2 <- read.csv(file = "Base2.csv", sep = ",", header = T)
 base2$Productoras <- as.factor(base2$Productoras)
 
 base2$Banco <- as.factor(base2$Banco)
-```
 
 
-```{r}
+## ------------------------------------------------------------------------------
 # Vamos a dar un ejejmplo del texto a generear y exportar:
 cat("\n\"Nombre\" trabaja en \"Productora\", donde le consignan en \"Banco\"")
-```
 
-```{r}
+
+## ------------------------------------------------------------------------------
 # Cambiemos los datos, pero antes debemos recordar que cambiar factoes, antes se deben agregar a los levels
 
 levels(base2$Banco) <- sub("Colpatria","Colpatria&Scotiabanck", levels(base2$Banco))
@@ -124,5 +93,4 @@ texto <- data.frame(
 
 write.table(texto, file = "BancosProductoras.txt", row.names = F)
 
-```
 
